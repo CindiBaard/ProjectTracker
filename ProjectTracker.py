08d71bd06c6b1a79057e0dc1514b2ff6f5343aba
CindiBaard <cindi@smallbizsystems.co.za>
2026-03-05 14:32:10 +0200

    Add files via upload

--- ProjectTracker.py
+++ ProjectTracker.py
@@ -96,7 +96,7 @@ def get_options(filename):
 DROPDOWN_CONFIG = {
     "Category": "Category.csv", "Length": "Length.csv", "Material": "Material.csv",
     "Orifice": "Orifice.csv", "Diameter": "TubeDia.csv", "Foiling": "Foiling.csv",
-    "Cap_Lid Style": "Cap_Lid Style.csv", "Machine": "Machine.csv", "Sales Rep": "Sales Rep.csv",
+    "Cap_Lid Style": "Cap_Lid style.csv", "Machine": "Machine.csv", "Sales Rep": "Sales Rep.csv",
     "Cap_Lid Material": "CapMaterial.csv", "Cap_Lid Diameter": "Cap_Lid Diameter.csv"
 }
 
