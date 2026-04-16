c10488c5fda06dc3aa43b258711ef639be36b992
CindiBaard <cindi@smallbizsystems.co.za>
2026-04-16 13:13:01 +0200

    Add files via upload

--- ProjectTracker.py
+++ ProjectTracker.py
@@ -409,6 +409,9 @@ elif tab_nav == "🌐 Cloud Sync":
             worksheet = spreadsheet.get_worksheet(0)
             cloud_data = pd.DataFrame(worksheet.get_all_records())
             if not cloud_data.empty:
+    # --- ADD THIS LINE HERE ---
+    # This forces the ID column to be text so '10017_1' doesn't break the save
+    cloud_data['Pre-Prod No.'] = cloud_data['Pre-Prod No.'].astype(str)
                 st.session_state.google_data = cloud_data
                 st.success("Successfully fetched data!")
         except Exception as e:
